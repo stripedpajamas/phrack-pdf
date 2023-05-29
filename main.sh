9642e57b7271ec68a0c9782e1c994662bdb156f5
Michael Rowley <michaellrowley@protonmail.com>
2023-05-29 02:33:49 +0100

    Incremented issue count

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(seq 69); do mkdir -p $i; curl -# http://phrack.org/archives/tgz/phrack$i.tar.gz | tar xf - -C $i; done
-for i in $(seq 69); do node index.js $i $i/*.txt; done
+for i in $(seq 70); do mkdir -p $i; curl -# http://phrack.org/archives/tgz/phrack$i.tar.gz | tar xf - -C $i; done
+for i in $(seq 70); do node index.js $i $i/*.txt; done
 mkdir -p pdfs
 mv *.pdf pdfs
